07fe964d37270eb6a4d6048f89695a601df8cd5a
TheCsarbeat <ytcesarjs@gmail.com>
2023-07-01 05:09:07 -0600

    Update sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,90 +1,22 @@
-import * as React from 'react'
+import * as React from "react"
 import { Link } from "gatsby"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
 
 const Sidebar = () => {
   return (
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <Link to="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span className="fs-5 d-none d-sm-inline">Menu</span>
-                </Link>
-                <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                    <li className="nav-item">
-                        <Link to="#" className="nav-link align-middle px-0">
-                        <FontAwesomeIcon icon={faHouse} /> <span className="ms-1 d-none d-sm-inline">Home</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </Link>
-                        <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <li className="w-100">
-                                <Link to="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1 </Link>
-                            </li>
-                            <li>
-                                <Link to="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2 </Link>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <Link to="#" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Orders</span></Link>
-                    </li>
-                    <li>
-                        <Link to="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
-                            <i className="fs-4 bi-bootstrap"></i> <span className="ms-1 d-none d-sm-inline">Bootstrap</span></Link>
-                        <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                            <li className="w-100">
-                                <Link to="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2</Link>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <Link to="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-grid"></i> <span className="ms-1 d-none d-sm-inline">Products</span> </Link>
-                            <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                            <li className="w-100">
-                                <Link to="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 2</Link>
-                            </li>
-                            <li>
-                                <Link to="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 3</Link>
-                            </li>
-                            <li>
-                                <Link to="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 4</Link>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <Link to="#" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-people"></i> <span className="ms-1 d-none d-sm-inline">Customers</span> </Link>
-                    </li>
-                </ul>
-                <hr/>
-                <div className="dropdown pb-4">
-                    <Link to="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle"/>
-                        <span className="d-none d-sm-inline mx-1">loser</span>
-                    </Link>
-                    <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-                        <li><a className="dropdown-item" href="#">New project...</a></li>
-                        <li><a className="dropdown-item" href="#">Settings</a></li>
-                        <li><a className="dropdown-item" href="#">Profile</a></li>
-                        <li>
-                            <hr className="dropdown-divider"/>
-                        </li>
-                        <li><a className="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img
+            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+            alt="Bootstrap"
+            width="30"
+            height="24"
+          />
+        </a>
+      </div>
+    </nav>
   )
 }
 
